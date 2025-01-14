2997f180caf9df8fb01c017cd03ceb6d0aa78971
Kantin FAGNIART <140246837+KANTIN-FAGN@users.noreply.github.com>
2025-01-14 20:50:49 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,35 @@
-// App.js
-import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import PublicRoute from "./routes/publicRoute/PublicRoute";
-import PrivateRoute from "./routes/privateRoute/PrivateRoute";
+import React, {useState} from "react";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import PublicRoute from "routes/publicRoute/PublicRoute";
+import PrivateRoute from "routes/privateRoute/PrivateRoute";
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
-// Importez vos pages/components
-import Home from "./scenes/home/Home";
-import Login from "./scenes/login/Login";
-import Dashboard from "./scenes/dashboard/Dashboard";
-import NotFound from "./scenes/notFound/NotFound";
+import Home from "scenes/home/Home";
+import Login from "scenes/login/Login";
+import Dashboard from "scenes/dashboard/Dashboard";
+import NotFound from "scenes/notFound/NotFound";
 
 const App = () => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [isAuthenticated] = useState(false);
+    return (
+        <Router>
+            <Routes>
+                {/* Routes publiques */}
+                <Route element={<PublicRoute isAuthenticated={isAuthenticated}/>}>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/login" element={<Login/>}/>
+                </Route>
 
-  return (
-      <Router>
-        <Routes>
-          {/* Routes publiques */}
-          <Route element={<PublicRoute isAuthenticated={isAuthenticated} />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-          </Route>
+                {/* Routes privées */}
+                <Route element={<PrivateRoute isAuthenticated={isAuthenticated}/>}>
+                    <Route path="/admin/dashboard" element={<Dashboard/>}/>
+                </Route>
 
-          {/* Routes privées */}
-          <Route element={<PrivateRoute isAuthenticated={isAuthenticated} />}>
-            <Route path="/admin/dashboard" element={<Dashboard />} />
-          </Route>
-
-          {/* Route pour toute page inexistante */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Router>
-  );
+                {/* Route pour toute page inexistante */}
+                <Route path="*" element={<NotFound/>}/>
+            </Routes>
+        </Router>
+    );
 };
 
 export default App;
