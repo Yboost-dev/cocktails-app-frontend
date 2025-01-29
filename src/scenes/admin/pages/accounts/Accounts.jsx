c5f680d1068ad2ef1574847f1d06a18e944732da
VitoDeriu <vito.deriu@ynov.com>
2025-01-29 15:57:50 +0100

    feat (admin): add accountService

--- src/scenes/admin/pages/accounts/Accounts.jsx
+++ src/scenes/admin/pages/accounts/Accounts.jsx
@@ -1,13 +1,33 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import NavBar from "../components/navBar/NavBar";
+import { getAllUsers } from "services/auth/accountService";
 
 const Accounts = () => {
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        getAllUsers()
+            .then((data) => {
+                setUsers(data);
+            })
+            .catch((error) => {
+                console.error("Une erreur est survenue, oupsi.. :", error)
+            })
+    }, []) 
 
     return (
         <div className="dashboard-global">
             <NavBar/>
             <p>Comptes</p>
-            
+            {users.map((user) => (
+                <div>
+                    <p>{user.id}</p>
+                    <p>{user.firstname}</p>
+                    <p>{user.lastname}</p>
+                    <p>{user.email}</p>
+                    <p>{user.role}</p>
+                </div>
+            ))}
         </div>
     );
 };
---
+++ src/services/auth/accountService.js
@@ -0,0 +1,18 @@
+const API_BASE_URL = process.env.REACT_APP_API_URL;
+
+export const getAllUsers = async () => {
+    const token = localStorage.getItem("token");
+    try {
+        const response = await fetch(`${API_BASE_URL}/users`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+            },
+        });
+        return await response.json();
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
