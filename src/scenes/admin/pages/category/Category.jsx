88bed64a72cc5d00e3a6574808330994613834c1
VitoDeriu <vito.deriu@ynov.com>
2025-01-29 15:03:21 +0100

    feat (admin) : add categoryService

--- src/scenes/admin/pages/category/Category.jsx
+++ src/scenes/admin/pages/category/Category.jsx
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NavBar from "../components/navBar/NavBar";
+import { getAllCategory } from "services/category/categoryService";
 
 const Category = () => {
+
+    const [categories, setCategory] = useState([]);
+
+    useEffect(() => {
+        getAllCategory()
+            .then((data) => {
+                setCategory(data);
+            })
+            .catch((error) => {
+                console.error("Une erreur est survenue :", error);
+            });
+    }, []);
 
     return (
         <div className="dashboard-global">
             <NavBar/>
             <p>Catégories</p>
-            
+            {categories.map((category) => (
+                <div key={category.id}>
+                    <p>{category.name}</p>
+                </div>
+            ))}
         </div>
     );
 };
